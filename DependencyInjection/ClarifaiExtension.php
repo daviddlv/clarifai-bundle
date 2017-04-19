7325db1039970691344675c78fc06b2364f7f1de
Vincent CHALAMON <vincentchalamon@gmail.com>
2017-04-19 13:18:07 +0200

    Clean code

--- DependencyInjection/ClarifaiExtension.php
+++ DependencyInjection/ClarifaiExtension.php
@@ -5,7 +5,7 @@ namespace ClarifaiBundle\DependencyInjection;
 use ClarifaiBundle\Client\Client;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -14,6 +14,31 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  */
 class ClarifaiExtension extends Extension
 {
+    /**
+     * @var array
+     */
+    private $classes = [];
+
+    /**
+     * Gets the classes to cache.
+     *
+     * @return array An array of classes
+     */
+    public function getClassesToCompile()
+    {
+        return $this->classes;
+    }
+
+    /**
+     * Adds classes to the class cache.
+     *
+     * @param array $classes An array of classes
+     */
+    public function addClassesToCompile(array $classes)
+    {
+        $this->classes = array_merge($this->classes, $classes);
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -25,9 +50,7 @@ class ClarifaiExtension extends Extension
         $authConfig = $config['auth'];
         unset($config['auth']);
 
-        $apiClient = new Definition(Client::class, [
-            $authConfig
-        ]);
+        $apiClient = new Definition(Client::class, [$authConfig]);
 
         $container->setDefinition('clarifai.api.client', $apiClient);
     }
